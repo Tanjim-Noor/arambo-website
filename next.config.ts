58e1b3913632cd721ab18d30ea6b5ac30eb0b65c
Tanjim Noor <tanjim.noor@g.bracu.ac.bd>
2025-09-27 19:54:39 +0600

    feat: added next js image config to accept host name i.ibb.co.com

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ibb.co.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
